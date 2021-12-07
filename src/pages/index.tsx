0030fcae78b69275aca7c64d01e56ed7d10b517d
Yudi Yamane <yudiyamane@gmail.com>
2021-12-07 20:50:34 -0300

    resolves #6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import styled from 'styled-components'
 import Link from '../components/Link'
 
 const Container = styled.div`
-  height: 100vh;
   width: 100vw;
   max-width: 100%;
   display: flex;
--- src/styles/pages/blog/about.ts
+++ src/styles/pages/blog/about.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100vh;
   width: 100vw;
   max-width: 100%;
   display: flex;
