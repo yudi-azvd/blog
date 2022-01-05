e00ae89f636049989b36f94015e53d81d462fd55
Yudi Yamane <yudiyamane@gmail.com>
2022-01-05 04:55:19 -0300

    added base path for next js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/',
-  //       permanent: true
-  //     }
-  //   ]
-  // }
+  basePath: '/blog'
 }
