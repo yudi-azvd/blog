85d51a240f51cff02dbf0d47f5e6eec832420f39
Yudi Yamane <yudiyamane@gmail.com>
2022-01-05 06:15:23 -0300

    fixed file as draft

--- next.config.js
+++ next.config.js
@@ -2,4 +2,20 @@
 module.exports = {
   reactStrictMode: true,
   basePath: '/blog',
+  // https://vercel.com/support/articles/can-i-redirect-from-a-subdomain-to-a-subpath
+  // https://estevanmaito.me/blog/how-to-redirect-subfolder-in-vercel
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [{
+            type: 'host',
+            value: 'yudi-azvd-blog.vercel.app'
+          }],
+          destination: '/blog/:path*'
+        }
+      ]
+    }
+  }
 }
